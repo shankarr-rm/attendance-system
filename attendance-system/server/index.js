db025db1b397f80495cb9af7ab2ddc2d1932a876
shankarr-rm <shankarrr.rm@gmail.com>
2024-07-31 00:04:06 +0530

    #3.2,JWT Token

--- attendance-system/server/index.js
+++ attendance-system/server/index.js
@@ -3,17 +3,20 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7000;
 
-// Middleware
+
 const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
+
 app.use('/api/auth', authRoutes);
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
--- attendance-system/server/routes/auth.js
+++ attendance-system/server/routes/auth.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const users = [
-  { email: 'shankar@gmail.com', password: '123456' },
-  { email: 'anujan@gmail.com', password: 'abc123' },
+  { email: 'shankar@gmail.com', password: bcrypt.hashSync('123456', 8) },
+  { email: 'anujan@gmail.com', password: bcrypt.hashSync('abc123', 8) },
 ];
+
+const secretKey = 'shan_sab_ac_lk'; 
 
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
   console.log('Received login request:', { email, password });
 
   // Check if the user exists
-  const user = users.find(u => u.email === email && u.password === password);
-
+  const user = users.find(u => u.email === email);
   if (user) {
-    // Authentication successful
+    // Check password
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Authentication successful, generate JWT token
+    const token = jwt.sign({ id: user.email }, secretKey, {
+      expiresIn: 86400, 
+    });
+
     console.log('Login successful:', user);
-    res.status(200).json({ message: 'Login successful', user });
+    res.status(200).json({ message: 'Login successful', token });
   } else {
     // Authentication failed
     console.log('Invalid credentials');
